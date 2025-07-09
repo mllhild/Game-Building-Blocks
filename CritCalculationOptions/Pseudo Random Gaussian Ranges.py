7606f2f7893a28a6e0d74ecab2a568206733ae06
mllhild <mllhild@gmail.com>
2025-07-09 12:38:10 +0200

    Create Pseudo Random Gaussian Ranges.py

---
+++ CritCalculationOptions/Pseudo Random Gaussian Ranges.py
@@ -0,0 +1,7 @@
+# 1. Reroll if two consecutive numbers differ by less than 0.02, like 0.875 and 0.856.
+# 2. Reroll if three consecutive numbers differ by less than 0.1, like 0.345, 0.421, and 0.387.
+# 3. Reroll if there is an increasing or decreasing run of 5 values, such as 0.342, 0.572, 0.619, 0.783, and 0.868.
+# 4. Reroll if too many numbers at the top or bottom of the range within the last N values, such as 0.325, 0.198, 0.056, 0.432, and 0.216.
+# 5. Reroll if there are four consecutive numbers that are all above or below zero.
+# 6. Reroll if there are four consecutive numbers that lie within the second or third deviations.
+# 7. Reroll if there are two consecutive numbers that lie within the third deviation.
