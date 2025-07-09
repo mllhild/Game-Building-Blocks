5f0fd7bf88ed526eb7e3bc9f252dc93acaa781f5
mllhild <mllhild@gmail.com>
2025-07-09 12:32:46 +0200

    Pseudo Random Integers.py

---
+++ CritCalculationOptions/Pseudo Rando Integers.py
@@ -0,0 +1,9 @@
+# Rules to make random numbers appear more random
+
+
+# 1. Repeating numbers, like “7, 7” or “3, 3”
+# 2. Repeating numbers separated by one digit, like “8, 3, 8” or “6, 2, 6”
+# 3. A counting sequence of 4 that ascends or descends, like “3, 4, 5, 6”
+# 4. Too many values at the top or bottom of a range within the last N values, like “6, 8, 7, 9, 8, 6, 9”
+# 5. Patterns of two numbers that appear in the last 10 values, like “5, 7, 3, 1, 5, 7”
+# 6. Too many of a particular number in the last 10 values, like “9, 4, 5, 9, 7, 8, 9, 0, 2, 9”
